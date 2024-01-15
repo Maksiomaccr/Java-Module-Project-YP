bd7c3e48fd64bab9b90b8855c3c90624cec0ad55
Maksim Vjugov <maxcraft.vjugov@yandex.ru>
2024-01-15 10:26:10 +0500

    Практическое задание №1 Исправленное

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -11,38 +11,75 @@ public class Calc {
 
 
     public void inputProducts(){
-        String answer = null;
+        String answer;
         String list = "Введённые товары:\n";
         while (true) {
             System.out.println("Введите название товара.");
-            product.name = scanner.next();
             scanner.useDelimiter("\n");
+            product.name = getName();
             System.out.println("Введите стоимость товара.");
             product.price = getDouble();
-            product.sum = product.sum + product.price;
+            product.sum += product.price;
             list = list + String.format("%s - %.2f %s\n", product.name, product.price, format.getRuble(product.sum));
-
             System.out.println("Товар добавлен. Добавить ещё товар или завершить?");
-            answer = scanner.next();
-
-            if(answer.equalsIgnoreCase("завершить")){
+            answer = getAnswer();
+            if (answer.equalsIgnoreCase("завершить")){
                 break;
             }
+
         }
         System.out.println(list);
     }
 
-    private static double getDouble(){
-        while(!scanner.hasNextDouble()){
-            System.out.println("Введите сумму");
-            scanner.nextLine();
-            while(scanner.nextDouble() <= 0){
-               System.out.println("Стоимость не может быть отрицательной. Введите стоимость корректно.");
-              scanner.nextLine();
-          }
-        }
+    private double getDouble() {
 
-        return scanner.nextDouble();
+        while (true) {
+            if (scanner.hasNextDouble()) {
+                double num = scanner.nextDouble();
+                if (num > 0) {
+                    return num;
+                } else {
+                    System.out.println("Стоимость не может быть отрицательной. Введите стоимость корректно.");
+                    scanner.nextLine();
+
+                }
+            } else if (!scanner.hasNextDouble()) {
+                System.out.println("Введите число");
+                scanner.next();
+            }
+
+        }
+    }
+
+    private String getName(){
+        String name;
+        while(true){
+            name = scanner.next();
+            if(name.trim().isEmpty() || name == null){
+                System.out.println("Введите пожалуйста название товара!");
+            }
+            else{break;}
+        }
+        return name;
+    }
+
+    private String getAnswer() {
+
+        while (true) {
+            if (scanner.hasNextLine()) {
+                String answer = scanner.next();
+                if (answer.equalsIgnoreCase("добавить") || answer.equalsIgnoreCase("завершить")) {
+                    return answer;
+                } else {
+                    System.out.println("Добавить ещё товар или завершить?");
+                    scanner.nextLine();
+                }
+            } else if (!scanner.hasNextLine()) {
+                System.out.println("Добавить ещё товар или завершить?");
+                scanner.nextLine();
+            }
+
+        }
     }
 
 
